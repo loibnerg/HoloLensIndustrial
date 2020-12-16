bcfaf8a73418c364e39b1438ffb516043f316ede
Gernot Loibner <lb@new.edvhtl.at>
2020-12-16 15:45:41 +0100

    added sqhere to provide position message to "mire" topic

---
+++ Assets/Scripts/SphereMqttDataLogger.cs
@@ -0,0 +1,36 @@
+﻿using M2MqttUnity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMqttDataLogger : M2MqttUnityClient
+{
+    public string topic;    
+
+    protected override void OnConnected()
+    {
+        Debug.Log("OnConnected");
+        StartCoroutine("LoggingCoroutine");
+    }
+
+
+    IEnumerator LoggingCoroutine()
+    {
+        while (true)
+        {
+            
+            yield return new WaitForSeconds(.1F);
+            try
+            {
+                string message = transform.position.x + "/" + transform.position.y + "/" + transform.position.z;
+
+                client.Publish(topic, System.Text.Encoding.Default.GetBytes(message));
+            }catch(Exception e)
+            {
+                Debug.LogError(e.StackTrace);
+                Debug.LogError(e.Message);
+            }
+        }
+    }
+}
