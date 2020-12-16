d89e87d85f118e8aa54c925782f09bfaebf100f4
Gernot Loibner <lb@htl-kaindorf.at>
2020-12-16 15:14:35 +0100

    Mqtt added

---
+++ Assets/Scripts/MyMqttClient.cs
@@ -0,0 +1,30 @@
+﻿using M2MqttUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMqttClient : M2MqttUnityClient
+{
+    protected override void SubscribeTopics()
+    {
+        Debug.Log("subscribe");
+        client.Subscribe(new string[] { "mire" }, new byte[] { 0 });
+    }
+
+    protected override void DecodeMessage(string topic, byte[] message)
+    {
+        Debug.Log("message received");
+        switch (topic)
+        {
+            case "mire":
+                string str = System.Text.Encoding.Default.GetString(message);
+                GetComponent<TextMesh>().text = str;
+                break;
+        }
+    }
+
+    protected override void UnsubscribeTopics()
+    {
+        client.Unsubscribe(new string[] { "mire" });
+    }
+}
